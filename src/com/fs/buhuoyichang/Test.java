cb4abecd3125f20434680edeb7818ce267cc5373
wohen <lixinfight@gmail.com>
2018-09-21 21:15:46 +0800

    catch 多个

--- src/com/fs/buhuoyichang/Test.java
+++ src/com/fs/buhuoyichang/Test.java
@@ -3,7 +3,7 @@ package com.fs.buhuoyichang;
 public class Test {
     public void div(int a, int b) {
         int c = 0;
-        try {//捕获异常
+        /*try {//捕获异常
             c = a / b;//可能发生异常
 
         } catch (ArithmeticException e) {//处理异常 catch 越往后级别越大
@@ -12,10 +12,19 @@ public class Test {
             c = a / b;
         } finally {//无论try执行还是catch执行都会执行finally,可以做一些收尾，或清理的工作
             System.out.println("c= " + c);
+        }*/
+        try {
+            c =a/b;
+
+        }catch (ArithmeticException e){
+
+
+
+
+    }catch (Exception e1){
+
         }
-
-
-    }
+        }
 
     public static void main(String[] args) {
         Test t = new Test();
